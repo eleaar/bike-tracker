72e668d475c3df443982ac610d21a5db1ecdb26b
Daniel Krzywicki <krzywicki.daniel@gmail.com>
2013-06-04 10:53:00 +0200

    fix notifications to be compatible with api7

--- src/org/biketracker/Launcher.java
+++ src/org/biketracker/Launcher.java
@@ -17,12 +17,12 @@
 package org.biketracker;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 
 public class Launcher extends Activity {
 
@@ -43,13 +43,16 @@ public class Launcher extends Activity {
 		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
 				resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
-				this).setSmallIcon(R.drawable.bike)
-				.setContentTitle("Bike tracker")
-				.setContentText("Click to stop tracking").setAutoCancel(true)
-				.setContentIntent(pendingIntent);
-
+		Notification note = new Notification(
+				R.drawable.bike, 
+				"Bike Tracker",
+				System.currentTimeMillis());
+		note.setLatestEventInfo(
+				this, 
+				"BikeTracker", 
+				"Click to stop tracking",
+				pendingIntent);
 		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		notificationManager.notify(0, mBuilder.build());
+		notificationManager.notify(0, note);
 	}
 }
